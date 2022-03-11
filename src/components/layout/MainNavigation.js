b3580a08fac1c7682a5106b1ce1819407949c54f
Mark D. Slaysman <mark_slaysman@yahoo.com>
2022-03-10 21:04:15 -0700

    Adjusted Navigation setup

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,4 +1,5 @@
 //REACT imports
+import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
@@ -7,13 +8,13 @@ function MainNavigation() {
     return (
         <Navbar bg="light" expand="sm">
             <Container fluid>
-                <Navbar.Brand href="/">Mark D. Slaysman</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">Mark D. Slaysman</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/housescrud">Houses-CRUD</Nav.Link>
-                        <Nav.Link href="/moviereview">Moview Review</Nav.Link>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/housescrud">Houses-CRUD</Nav.Link>
+                        <Nav.Link as={Link} to="/moviereview">Moview Review</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
